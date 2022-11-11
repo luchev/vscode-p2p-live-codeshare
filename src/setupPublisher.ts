ae0a125cc053bbbad1011d1aaad6aecf31d9a167
Laursen <201703930@post.au.dk>
2022-11-11 14:39:20 +0100

    direct stream

--- src/setupPublisher.ts
+++ src/setupPublisher.ts
@@ -1,9 +1,12 @@
 import * as vscode from 'vscode';
+import {toString as uint8ArrayToString} from "uint8arrays/to-string";
 import {fromString as uint8ArrayFromString} from "uint8arrays/from-string";
 import {createNode} from './shared/createNode';
 import {Topics} from './shared/constants';
 import { createFromProtobuf } from '@libp2p/peer-id-factory';
 import { peer1 } from './shared/peers';
+import {Connection} from '@libp2p/interface-connection';
+import {pipe}from 'it-pipe';
 
 async function setupPublisher(ctx: vscode.ExtensionContext) {
 	const peerid = await createFromProtobuf(uint8ArrayFromString(peer1, "base64"));
@@ -16,6 +19,16 @@ async function setupPublisher(ctx: vscode.ExtensionContext) {
 			vscode.window.showInformationMessage(err);
 		});
 	}, 1000);
+
+	node2.connectionManager.addEventListener("peer:connect", async (evt) => {
+		const connection = evt.detail as Connection;
+		console.log(connection.streams)
+		const stream = await connection.newStream("/zip");
+		pipe(
+			[uint8ArrayFromString("hello")],
+			stream);
+	});
+	
 }
 
 export function registerSetupPublisher(ctx: vscode.ExtensionContext) {
--- src/setupSubscriber.ts
+++ src/setupSubscriber.ts
@@ -8,6 +8,8 @@ import {logger} from './shared/logger';
 import { Message } from '@libp2p/interface-pubsub';
 import { peer2 } from './shared/peers';
 import { createFromProtobuf } from '@libp2p/peer-id-factory';
+import {Connection} from '@libp2p/interface-connection';
+import {pipe}from 'it-pipe';
 
 async function setupSubscriber(ctx: vscode.ExtensionContext) {
 	const peerid = await createFromProtobuf(uint8ArrayFromString(peer2, "base64"));
@@ -20,6 +22,18 @@ async function setupSubscriber(ctx: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage(`node1 received: ${uint8ArrayToString(evt.detail.data)} on topic ${evt.detail.topic}`);
 	});
 	node1.pubsub.subscribe(topic);
+
+	node1.handle('/zip', ({stream, connection}) => {
+		console.log(`Message from ${connection.remotePeer}`);
+		pipe(
+			stream,
+			async function (source) {
+				for await (const msg of source) {
+					console.log(uint8ArrayToString(msg.subarray()));
+				}
+			}
+		);
+	});
 
 	const inputAddress = await vscode.window.showInputBox({
 		placeHolder: "Multiaddress",
