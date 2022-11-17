b4756ce78985d753ddc432546b31c539a000c66b
LazyTurtleknight <35065432+LazyTurtleknight@users.noreply.github.com>
2022-11-17 12:09:56 +0100

    Add recursion to file/directory creation.

--- src/shared/actions/workspace/create-directory.ts
+++ src/shared/actions/workspace/create-directory.ts
@@ -1,14 +1,23 @@
-import { existsSync, mkdirSync, writeFileSync } from "fs";
+import { existsSync, mkdirSync} from "fs";
 import path from "path";
-import { workspace } from "vscode";
-import { CreateDirectoryEvent, CreateFileEvent } from "../../events/workspace";
+import { CreateDirectoryEvent} from "../../events/workspace";
 import {WorkspaceEvent} from "../../events/workspace/event";
 import {getWorkspaceRoot} from "../../workspace-path";
 
 export function handleCreateDirectory(message: WorkspaceEvent) {
   const params = message as CreateDirectoryEvent;
   const absolutePath = path.join(getWorkspaceRoot(), ...params.path);
+  ensureDirectoryExistence(absolutePath);
   if (!existsSync(absolutePath)) {
     mkdirSync(absolutePath);
   }
+}
+
+export function ensureDirectoryExistence(filePath: string) {
+  var dirname = path.dirname(filePath);
+  if (existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  mkdirSync(dirname);
 }
