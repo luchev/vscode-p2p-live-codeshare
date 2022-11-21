8aab1046e2a8f613954aee5f6292a3bbc9059acd
Ivan Luchev <luchevz@gmail.com>
2022-11-21 22:36:37 +0100

    Remove unused imports

--- src/shared/state/peer.ts
+++ src/shared/state/peer.ts
@@ -1,10 +1,5 @@
-import { Libp2p } from "libp2p";
 import { Peer } from "../entity/peer";
-import { toHumanReadableName } from "../nameGenerator";
 
-let _p2pPeer: Libp2p;
-let _peerName: string;
-let _isDELETEMEInitialized = false;
 let _isInitialized = false;
 let _peer: Peer;
 
